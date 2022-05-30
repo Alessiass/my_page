f4af70ef96c0fe15765cf065ba71ad2d3d26ca55
Alessias <94013800+Alessiass@users.noreply.github.com>
2022-05-30 16:47:47 +0200

    Initial commit

---
+++ src/Navigation.js
@@ -0,0 +1,13 @@
+export default function Navigation(){
+    return    <nav className="navigationbar" >
+        <a href="/" className="sub-page"> Site name </a>
+        <ul>
+            <li>
+                <a href="/about_me">About me</a>
+            </li>
+            <li>
+                 <a href="/contant">Contact</a>
+            </li>
+        </ul>
+    </nav>
+}
