a6b457da8867958c352109246fccfe0db3ecd0ca
Alessias <94013800+Alessiass@users.noreply.github.com>
2022-05-30 19:16:31 +0200

    Improved navbar with NavLink from react-dom

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,21 +1,17 @@
-import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 export default function Navigation(){
     return    <nav className="navigationbar" >
-        <Link to="/" className="sub-page"> Site name </Link>
+        <NavLink to="/" className="sub-page"> Site name </NavLink>
         <ul>
-                <ActiveLink to="/about_me">About me</ActiveLink>
-                <ActiveLink to="/contact">Contact</ActiveLink>
+            <NavigationTabs to="/about_me">About me</NavigationTabs>
+            <NavigationTabs to="/contact">Contact</NavigationTabs>
         </ul>
     </nav>
 }
 
-function ActiveLink({to, children, ...props}){ //Sets the current window as active and generates the lists
-    const resolvedPath = useResolvedPath(to) //Converts relative path to absolute
-    const isActive = useMatch({ path: resolvedPath.pathname, end: true}) //Full match
-    return (
-        <li className = {isActive ? "active" : ""}>
-            <Link to={to} {...props}>{children}</Link>
-        </li>
-    )
+function NavigationTabs ({to, children, ...props}) {
+    return (<li>
+        <NavLink to={to}{...props}>{children}</NavLink>
+    </li>)
 }
